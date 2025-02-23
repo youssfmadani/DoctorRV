1b750304092638602e6e4223581b666444d3e719
youssfmadani <youssfelmadani@gmail.com>
2025-02-23 16:16:18 +0000

    Ajout de UserServlet.

---
+++ src/main/java/conn/user/servlet/UserRegister.java
@@ -0,0 +1,50 @@
+package conn.user.servlet;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import conn.daw.Userdaw;
+import conn.db.DBconnect;
+import conn.entity.User ; 
+@WebServlet("/user_register")
+
+public class UserRegister extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			String fullname=req.getParameter("fullname");
+			String email=req.getParameter("email");
+			String password=req.getParameter("password");
+			
+			
+			User u= new User(fullname,email,password); 
+Userdaw daw= new Userdaw(DBconnect.getConn()) ; 
+HttpSession session=req.getSession();
+boolean f= daw.UserRegister(u); 
+
+if (f) {
+
+	session.setAttribute("sucMsg", "Register Sucessfully");
+	resp.sendRedirect("signup.jsp");
+
+} else {
+	session.setAttribute("errorMsg", "Something wrong on server");
+	resp.sendRedirect("signup.jsp");
+}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	
+	
+}
